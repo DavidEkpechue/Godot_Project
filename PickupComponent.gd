8783daca62de665349c474f70ce17d3f02732d4d
DavidEkpechue <davidekpechue@gmail.com>
2024-01-28 00:15:49 +0000

    PARTICLE COMPONENT

--- PickupComponent.gd
+++ PickupComponent.gd
@@ -2,20 +2,28 @@ extends Node2D
 class_name PickupComponent
 
 @export var pickup_area: Area2D
+@export var pickup_efect: GPUParticles2D
+@export var sprite: Sprite2D
 
 func _ready():
 	if pickup_area:
 		pickup_area.connect("body_entered", Callable(self, "_on_body_entered"))
+		
+
+func play_particle():
+	pickup_efect.emitting = true
+	sprite.visible = false
+		
+	await(get_tree().create_timer(pickup_efect.lifetime).timeout)
+	get_parent().queue_free() 
+
 
 func _on_body_entered(body):
-	var stats = body.get_node_or_null("PlayerStatsComponent")  # Adjust the node path as necessary
-
-	
-	if stats:
-		stats.add_coins(2)
-			
-	if get_parent():  # Check if the PickupComponent has a parent before trying to delete it
-		print('picked up: ', get_parent().name)
-		get_parent().queue_free()  # This will free the parent node, which includes this PickupComponent
+	if body.has_node("PlayerStatsComponent"):
+		var stats = body.get_node("PlayerStatsComponent")
+		stats.add_coins(2)  # Add coins to the player stats
+		play_particle()  # Call the function to play the particle effect
+		
 	else:
-		queue_free()  # If there is no parent, just free the PickupComponent itself
+		# If the body is not the player, just queue the node for deletion
+		queue_free()
